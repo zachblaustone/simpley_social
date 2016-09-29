0124e7b9a546e82d4ac08622882839cbbfcd700c
ZachBlaustone <Zachblaustone@gmail.com>
2016-09-29 12:34:43 -0700

    Spet_29th_12:34
    Finished the UI on the sign in page

---
+++ simpley_social/MaterialLongButton.swift
@@ -0,0 +1,22 @@
+//
+//  MaterialLongButton.swift
+//  simpley_social
+//
+//  Created by Zachary Blaustone on 9/29/16.
+//  Copyright © 2016 Pryzm. All rights reserved.
+//
+
+import UIKit
+
+class MaterialLongButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+    }
+}
